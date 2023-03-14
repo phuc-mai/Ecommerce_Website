83d75d6e1813e6aa504c32bce194992611f39414
Maria Mai <mariamaihong1309@gmail.com>
2023-03-13 21:59:28 -0400

    second release

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
+import Link from 'next/link'
 
 const Footer = () => {
     return (
         <div className='footer-container'>
             <p>© 2023 Phuc Mai Electronics</p>
             <p className='icons'>
-                <AiFillInstagram />
-                <AiOutlineTwitter />
+                <Link href="https://www.instagram.com/">
+                    <AiFillInstagram />
+                </Link>
+                <Link href='https://twitter.com/?lang=en'>
+                    <AiOutlineTwitter />
+                </Link>
             </p>
         </div>
     )
